d1ba8c5fb757ddb9ab78d1c7b0d7e1394a855b08
awanmbandi <awanmbandi@gmail.com>
2026-05-18 18:57:19 -0400

    updated s3 config

--- deploy-s3/main.tf
+++ deploy-s3/main.tf
@@ -1,6 +1,6 @@
 # Resources = Bucket resource
 resource "aws_s3_bucket" "mbandi_dev_bucket" {
-  bucket = "mbandi-dev-bucket-126543279054"
+  bucket = "mbandi-dev-bucket-126543279878787"
   tags = {
     Environment = "dev"
     CostCenter  = "cc590"
